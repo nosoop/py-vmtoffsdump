25c24afcd5b04d25b372bd77b883c81a609ab35d
nosoop <nosoop@users.noreply.github.com>
2021-05-13 22:50:26 -0700

    Convert dumper into a class

--- vmtoffsdump/__main__.py
+++ vmtoffsdump/__main__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 from . import dumper
+import argparse
+
+import lief
 import pydemangler
 
 def demangle(input):
@@ -9,13 +12,20 @@ def demangle(input):
 	}
 	return pydemangler.demangle(input) or fallbacks.get(input, input)
 
-def render_vtable(typename):
-	linux_vmt, *_ = dumper.get_class_vtables(typename)
-	windows_vmt = list(dumper.guesstimate_windows_mapping(typename))
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('binary', help = "Binary file to validate", type = lief.parse)
+	args = parser.parse_args()
 	
-	for n, f in enumerate(linux_vmt):
-		try:
-			wi = windows_vmt.index(f)
-		except ValueError:
-			wi = None
-		print('L:{:3d}'.format(n), 'W:{:3d}'.format(wi) if wi is not None else '     ', demangle(f.name))
+	vtable_handler = dumper.VTableDumper(args.binary)
+	
+	def render_vtable(typename):
+		linux_vmt, *_ = vtable_handler.get_class_vtables(typename)
+		windows_vmt = list(vtable_handler.guesstimate_windows_mapping(typename))
+		
+		for n, f in enumerate(linux_vmt):
+			try:
+				wi = windows_vmt.index(f)
+			except ValueError:
+				wi = None
+			print('L:{:3d}'.format(n), 'W:{:3d}'.format(wi) if wi is not None else '     ', demangle(f.name))
